c9a2d56a9231f1ade0235873c132545d5d003a96
ronrun <arhatron@gmail.com>
2025-03-02 20:58:03 +0800

    測試

--- src/LaravelOpencartAdminServiceProvider.php
+++ src/LaravelOpencartAdminServiceProvider.php
@@ -3,21 +3,26 @@
 namespace Ronrun\LaravelOpencartAdmin;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Log;
 
 class LaravelOpencartAdminServiceProvider  extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('opencart', function () {
-            return new \Ronrun\LaravelOpencartAdmin\LaravelOpencartAdmin();
-        });
     }
 
     public function boot()
     {
-        // 如果有 routes、config、migrations，可以在這裡載入
-        $this->publishes([
-            __DIR__ . '/../config/opencartadmin.php' => config_path('opencartadmin.php'),
-        ], 'config');
+        // Log::info('LaravelOpencartAdminServiceProvider boot() loaded.');
+
+        // // 如果有 routes、config、migrations，可以在這裡載入
+        // $this->publishes([
+        //     __DIR__ . '/../config/opencartadmin.php' => config_path('opencartadmin.php'),
+        // ], 'config');
+
+        // // 發布資源
+        // $this->publishes([
+        //     __DIR__.'/../../public' => public_path('vendor/laravel-opencartadmin'),
+        // ], 'laravel-opencartadmin-assets');
     }
 }

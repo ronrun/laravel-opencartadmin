507a3d38f2498f3af18c34ad600c6a0183faf815
ronrun <arhatron@gmail.com>
2025-03-02 22:09:50 +0800

    加入資源檔

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('ocadmin')->group(function () {
     Route::get('/', function () {
-        return 123;
+        return view('ocadmin.app');
     });
 });
